206f7cad110b44cbca1a181ef227303de8e7b43b
moseskippe <mkippe@credable.io>
2023-10-13 00:27:01 +0300

    changed image😅

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -8,7 +8,7 @@ export default function ArticleCard() {
         <Image
           alt="Card background"
           className="object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1542027959157-98e6745f4ba7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
+          src="https://images.unsplash.com/photo-1664575601786-b00156752b61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
           width={270}
         />
       </CardBody>
