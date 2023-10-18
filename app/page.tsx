b03c37479c7fd2f8d8c5592461e93d19c5cfbaaf
Hardtech <hardtech@Hardtechs-MacBook-Pro.local>
2023-10-18 11:08:23 +0300

    new changes made for the overall project

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
-'use client'
-import SearchBar from "./components/SearchBar"
-export default function Home() {
+import React from "react";
+import BlogPost from "./components/blog-post";
+import Button from "./components/button";
+import blogPosts from "./blogData";
+function page() {
+  
   return (
-    <div className="flex flex-row px-22 w-3/4 mt-2 mx-auto">
-      <p className="text-lg font-bold p-2">RamaniAuthors</p>
-      <div className="block w-full" >
-        <SearchBar />
-      </div>
+    <main className="flex w-full flex-col items-center justify-center gap-[100px]">
+      <section className="flex flex-row flex-wrap items-center justify-center gap-[60px] px-[80px]">
+        {blogPosts.map((post, index) => (
+          <BlogPost
+            key={index}
+            title={post.title}
+            authorName={post.authorName}
+            date={post.date}
+            shares={post.shares}
+            imageSrc={post.imageSrc}
+            imageAlt={post.imageAlt}
+            content={post.content}
+          />
+        ))}
+      </section>
 
-    </div>
-  )
+      <section className="w-[240px] ">
+        <Button  text="Load More" />
+      </section>
+    </main>
+  );
 }
+
+export default page;
